f04bb31813ff13b0e28c5189573fe5649bd833cc
Hemraj-7 <mhemraj814@gmail.com>
2026-03-13 23:15:06 +0530

    Recursion in java

--- Recursion1.java
+++ Recursion1.java
@@ -8,7 +8,37 @@ public class Recursion1 {
         printNumber(n - 1); // recursion
     }
 
+    // Print Numbers from 1 to 5
+    public static void printNumber2(int n) {
+        if (n == 6)
+            return;
+        System.out.println(n);
+        printNumber2(n + 1);
+    }
+
+    // Print sum of first n natural numbers
+    public static void printSum(int i, int n, int sum) {
+        if (i == n) {
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+        sum += i;
+        printSum(i + 1, n, sum);
+
+        System.out.println(i);
+    }
+
     public static void main(String args[]) {
+
+        System.out.println("Numbers from 5 to 1:");
         printNumber(5);
+
+        System.out.println("Numbers from 1 to 5:");
+        int n = 1;
+        printNumber2(n);
+
+        System.out.println("Sum of n natural numbers: ");
+        printSum(1, 5, 0);
     }
 }
